e5b70123aa4c088094b59f4552491a1e4dcc401c
Oleh Krehel <ohwoeowho@gmail.com>
2017-10-28 22:36:21 +0200

    orca.el: Fix link

--- orca.el
+++ orca.el
@@ -51,7 +51,7 @@
 (defcustom orca-handler-list
   (let ((emacs "~/Dropbox/org/wiki/emacs.org")
         (entor "~/Dropbox/org/ent.org"))
-    `((orca-handler-match-url "https://www.reddit.com/" ,emacs "Reddit")
+    `((orca-handler-match-url "https://www.reddit.com/r/emacs" ,emacs "Reddit")
       (orca-handler-match-url "https://emacs.stackexchange.com/" ,emacs "\\* Questions")
       (orca-handler-current-buffer "\\* Tasks")
       (orca-handler-file ,entor "\\* Articles")))
